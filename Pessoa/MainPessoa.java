b1c67c19e92e71f9ebb7e4fa0207a45349daa3af
Henrique2025 <160058681+Henrique2025@users.noreply.github.com>
2025-02-24 14:24:33 +0000

    atv

--- Pessoa/MainPessoa.java
+++ Pessoa/MainPessoa.java
@@ -4,7 +4,6 @@ public class MainPessoa {
     public static void main(String[] args) {
     
         Cliente cliente1 = new Cliente("Marta", "21", "30/12/24", "Cartão");
-        
         System.out.println(cliente1.toString());
 
         Funcionario funcionario1 = new Funcionario("José", "27", "0000", "Gerente", 2.000);
