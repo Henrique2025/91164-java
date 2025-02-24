fc44eef0dd333faa0a26ce8a640b871a21592b61
Henrique2025 <160058681+Henrique2025@users.noreply.github.com>
2025-02-24 14:23:14 +0000

    Salvar

---
+++ Pessoa/Cliente.java
@@ -0,0 +1,45 @@
+package Pessoa;
+
+public class Cliente extends Pessoa {
+    private String dataDaCompra;
+    private String formaDePagamento;
+    
+    
+
+    public Cliente(String dataDaCompra, String formaDePagamento) {
+        this.dataDaCompra = dataDaCompra;
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    public Cliente(String nome, String idade, String dataDaCompra, String formaDePagamento) {
+        super(nome, idade);
+        this.dataDaCompra = dataDaCompra;
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    public String getDataDaCompra() {
+        return dataDaCompra;
+    }
+
+    public void setDataDaCompra(String dataDaCompra) {
+        this.dataDaCompra = dataDaCompra;
+    }
+
+    public String getFormaDePagamento() {
+        return formaDePagamento;
+    }
+
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", dataDaCompra=" + dataDaCompra + ", idade=" + idade + ", formaDePagamento="
+                + formaDePagamento + "]";
+    }
+    
+
+    
+
+}
---
+++ Pessoa/Pessoa.java
@@ -0,0 +1,41 @@
+package Pessoa;
+
+public abstract class Pessoa {
+    protected String nome;
+    protected String idade;
+   
+      
+    public Pessoa() {
+    }
+
+
+    public Pessoa(String nome, String idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+    public String getIdade() {
+        return idade;
+    }
+
+
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    
+
+}
