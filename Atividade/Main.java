ea9e6b2b3ddeca7e8fef95c887a6b39587f3f708
Henrique2025 <160058681+Henrique2025@users.noreply.github.com>
2025-02-24 12:47:50 +0000

    Aula

---
+++ Atividade/Main.java
@@ -0,0 +1,16 @@
+public class Main { 
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Claudio", "22", "Salvador");
+        System.out.println(cliente.toString());
+
+        Endereco endereco = new Endereco("Rua A", "83", "Salvador");
+        System.out.println(endereco.toString());
+
+
+
+    }
+    
+    
+
+
+}
